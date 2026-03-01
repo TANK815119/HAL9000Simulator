02c38cd863afe3d2af4f62057866b83de5cbd4ce
Reid Jacobsen <52799675+TANK815119@users.noreply.github.com>
2026-03-01 02:31:22 -0600

    More handle bars

--- HAL9000Simulator/Assets/ButtonInstantiatePrefab.cs
+++ HAL9000Simulator/Assets/ButtonInstantiatePrefab.cs
@@ -7,9 +7,14 @@ public class ButtonInstantiatePrefab : MonoBehaviour, ButtonActionInterface
 {
     [SerializeField] private GameObject prefab;
     [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject HAL90001;
+    [SerializeField] private GameObject HAL90002;
 
     public void Play()
     {
         Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
+        Destroy(HAL90001);
+        Destroy(HAL90002);
+        Destroy(this.gameObject);
     }
 }
